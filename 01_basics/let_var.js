e507778c499ff626c083e5c12ca56deae626de9b
KedarMusale <musalekedar4@gmail.com>
2025-03-25 17:43:05 +0000

    container config done

---
+++ 01_basics/let_var.js
@@ -0,0 +1,10 @@
+const accountid=23
+//we cannot change value of a variable after declaring it as constant i.e const
+let accountname="Kedar"
+let accountemail="kedar@gmail.com"
+accountcity="Jaipur"
+var accountpassword=12345;
+let accountedu;
+//avoid using 'var' in JS. use 'let'
+console.table([accountid,accountname,accountemail,accountpassword,accountedu]);
+
